3942407d016a12c68ff1d13a73d437dc6f4d4137
Rafaella Buzatu <rafaella7260@yahoo.ro>
2023-09-29 17:44:13 +0200

    pyDESeq2 in bulk

--- bulk.py
+++ bulk.py
@@ -11,6 +11,7 @@ import utils.preprocessing as prep
 import sys
 import pandas as pd
 import scanpy as sc
+import pydeseq2
 sc.settings.set_figure_params(dpi=100, facecolor='white')
 
 
@@ -19,12 +20,12 @@ nCores = 40 # max 40
 nRAM = 100 # max 200
 
 
-pathToFastqFiles = 'data/bulk_RNAseq/raw'
+pathToFastqFiles = 'data/ytTutorial/raw'
 
 #FASTQC
 prep.doFastQC (pathToFastqFiles, nCores)
 
-#Quality control
+####Quality control
 pathToRefAdapters = 'data/resources/adapters.fa'
 pathToRefPhix = 'data/resources/phix174_ill.ref.fa.gz'
 
@@ -37,7 +38,7 @@ pathToProcessed = prep.bulkPreprocessing (pathToFastqFiles, pathToRefAdapters,
                                           pathToRefPhix, trimQuality = 10, 
                                           minLen = 30, nRAM= 100)
 
-#Alignment
+###Alignment
 
 #Create Genome Indices
 pathToGenomeReference  = 'data/resources/GCF_000001405.40_GRCh38.p14_genomic.fna'
@@ -54,9 +55,39 @@ pathToAlignment = prep.runSTARaligner (pathToProcessed, pathToGenomeDir, nCores 
 countMatrix = prep.extractCountMatrixBulk (pathToAlignment)
 #Read count matrix from file
 
+
+
+### DIFFERENTIAL EXPRESSION
 pathToCountMatrix = pathToFastqFiles.split('raw')[0]+'countMatrix.csv'
 countMatrix = pd.read_csv(pathToCountMatrix, index_col = 0)
 
-#Read count matrix into AnnData object
-adata= sc.AnnData(countMatrix.T)
+#Remove genes where counts are 0 for all samples
+countMatrix = countMatrix[countMatrix.sum(axis = 1) >0]
+#Transpose
+countMatrix = countMatrix.T
 
+#metadata dataframe
+#index: sample name
+#metadata = 
+
+#Read count matrix into AnnData object
+dds = pydeseq2.dds.DeseqDataSet(counts = countMatrix,
+                                clinical = metadata,
+                                design_factors= "condition") #column name from from the metadata table 
+#Run differential expression 
+dds.deseq2()
+
+#Extract data
+statRes = pydeseq2.ds.DeseqStats(dds, n_cpus = nCores, 
+                                 contrast = ('condition',
+                                             'condition1', 'condition2')) #values of column
+statRes.summary()
+#Get results into a dataframe
+DEgenes = statRes.results_df
+
+#Extract differentially expressed genes
+DEgenes = DEgenes[(DEgenes.padj < 0.05) & (abs(DEgenes.log2FoldChange) > 0.5) &(DEgenes.baseMean>=10)]
+
+#Do PCA
+sc.tl.pca(dds)
+sc.pl.pca(dds, color = 'condition')
